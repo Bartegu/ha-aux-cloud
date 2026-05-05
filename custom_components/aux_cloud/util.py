396de74a773f67941d7675097d44c9edaf7d20a2
Bartosz Iwaniec <bartek.iwaniec@gmail.com>
2026-05-05 17:36:45 +0200

    Implement fallback to cached parameters for improved availability handling.

--- custom_components/aux_cloud/util.py
+++ custom_components/aux_cloud/util.py
@@ -3,7 +3,7 @@ from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC, Device
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .api.const import AuxProducts
-from .const import _LOGGER, DOMAIN, MANUFACTURER
+from .const import _LOGGER, DOMAIN, MANUFACTURER, MAX_FAILED_POLLS
 
 
 class BaseEntity(CoordinatorEntity):
@@ -17,6 +17,10 @@ class BaseEntity(CoordinatorEntity):
         self._attr_unique_id = (
             f"{DOMAIN}_{self._device_id.lstrip('0')}_{self.entity_description.key}"
         )
+
+        self._cached_params = self._device.get("params", {}) if self._device else {}
+        self._failed_poll_count = 0
+        self._max_failed_polls = MAX_FAILED_POLLS
 
     @property
     def unique_id(self):
@@ -41,10 +45,15 @@ class BaseEntity(CoordinatorEntity):
     @property
     def available(self):
         """Return True if entity is available."""
+        current_params = self._device.get("params", {}) if self._device else {}
+
+        has_fresh_data = len(current_params) > 0
+        has_valid_cache = len(self._cached_params) > 0 and self._failed_poll_count <= self._max_failed_polls
+
         return (
-            self._device is not None
-            and self._device.get("endpointId") is not None
-            and len(self._device.get("params", {}).keys()) > 0
+                self._device is not None
+                and self._device.get("endpointId") is not None
+                and (has_fresh_data or has_valid_cache)
         )
 
     @callback
@@ -57,11 +66,43 @@ class BaseEntity(CoordinatorEntity):
         self.async_write_ha_state()
 
     def _get_device_params(self):
-        return self._device.get("params", {})
+        """Get device parameters, falling back to cache if the API drops a packet."""
+        current_params = self._device.get("params", {})
+        device_name = self._device.get("friendlyName", self._device_id)
+
+        if current_params and len(current_params) > 0:
+            if self._failed_poll_count > 0:
+                _LOGGER.info(
+                    "Device %s connection restored after %s failed attempts.",
+                    device_name,
+                    self._failed_poll_count
+                )
+            self._failed_poll_count = 0
+            self._cached_params = current_params
+            return current_params
+
+        self._failed_poll_count += 1
+
+        if self._cached_params and self._failed_poll_count <= self._max_failed_polls:
+            _LOGGER.warning(
+                "Missing params for device %s (Attempt %s/%s). Using cached parameters to prevent flapping.",
+                device_name,
+                self._failed_poll_count,
+                self._max_failed_polls
+            )
+            return self._cached_params
+        if self._failed_poll_count == self._max_failed_polls + 1:
+            _LOGGER.error(
+                "Device %s has not returned valid parameters for %s consecutive polls. Marking as unavailable.",
+                device_name,
+                self._failed_poll_count
+            )
+
+        return {}
 
     async def _set_device_params(self, params: dict):
         """Set parameters on the device."""
-        _LOGGER.debug("Setting %s for device %s", params, self._device["friendlyName"])
+        _LOGGER.debug("Setting %s for device %s", params, self._device.get("friendlyName", "AUX"))
 
         await self.coordinator.api.set_device_params(self._device, params)
         await self.coordinator.async_request_refresh()
